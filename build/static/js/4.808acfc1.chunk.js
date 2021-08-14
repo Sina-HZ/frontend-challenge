cc076ac0c91a3cece8d78547fbb02cdfed65b17f
sina hoseinzadeh <sina.hoseinzadeh@gmail.com>
2021-08-14 13:56:55 +0430

    add github page config

---
+++ build/static/js/4.808acfc1.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpfrontend-challenge"]=this["webpackJsonpfrontend-challenge"]||[]).push([[4],{163:function(e,n,r){"use strict";r.r(n);var t=r(94),c=r(3);n.default=function(){return Object(c.jsx)("div",{children:Object(c.jsx)(t.a,{children:"404 Error"})})}}}]);
+//# sourceMappingURL=4.808acfc1.chunk.js.map
