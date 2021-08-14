038fa4e4871b9dd3915d3e6c9d03a222c40fd6b2
sina hoseinzadeh <sina.hoseinzadeh@gmail.com>
2021-08-14 17:59:17 +0430

    fix image problem

--- src/pages/Newsletter/index.tsx
+++ src/pages/Newsletter/index.tsx
@@ -91,7 +91,7 @@ const Newsletter: React.FC = (props) => {
 
                 </Box>
                 <Box className={classes.imageSide}>
-                    <img src={process.env.PUBLIC_URL + '/frontend-challenge/assets/images/bg-newsletter.jpg'} alt='form bg' className={classes.bgImg} />
+                    <img src={'/frontend-challenge/assets/images/bg-newsletter.jpg'} alt='form bg' className={classes.bgImg} />
                 </Box>
             </Paper>
 
